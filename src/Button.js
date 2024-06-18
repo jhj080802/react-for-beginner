7273410f3414fce1dbd6e5118f33a10ddd891668
hyojoo <jhj080802@naver.com>
2024-06-18 20:20:36 +0900

    #5.1 Tour of CRA (with css - 5:17)

--- src/Button.js
+++ src/Button.js
@@ -1,6 +1,13 @@
+import PropTypes from "prop-types";
+
 function Button({text}){
     return <button>{text}</button>;
 }
+
+Button.PropTypes = {
+    text: PropTypes.string.isRequired,
+}
+
 // export : 다른 파일에서 사용할 수 있도록 내보내는 키워드
 // default : 이 모듈에서 기본적으로 내보내는 값을 지정 (하나의 값만 내보낼 수 있음)
 // Button : 내보내는 값
