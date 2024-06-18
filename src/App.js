152384c63ddbd80c425c4ebf35bbfecb50b3be9b
hyojoo <jhj080802@naver.com>
2024-06-18 20:05:59 +0900

    #5.1 Tour of CRA (2:39)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+//./Button 파일에서 기본으로 내보낸 Button 컴포넌트를 가져옴
+import Button from "./Button";
 function App() {
   return (
     <div>
       <h1>Welcome back!</h1>
+      <Button text={"Continue"}/>
     </div>
   );
 }
