657f730ab890e2c96649e92ad5c6c2d1569e7e1f
hyojoo <jhj080802@naver.com>
2024-06-18 21:18:05 +0900

    end #5.1 Tour of CRA

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-//./Button 파일에서 기본으로 내보낸 Button 컴포넌트를 가져옴
 import Button from "./Button";
+import styles from "./App.module.css";
+
 function App() {
   return (
     <div>
-      <h1>Welcome back!</h1>
+      <h1 className={styles.title}>Welcome back!</h1>
       <Button text={"Continue"}/>
     </div>
   );
 }
+
 export default App;
