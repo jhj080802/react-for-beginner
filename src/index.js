cc23eb6a72be95f8c17acd8b5ff50865db097da9
hyojoo <jhj080802@naver.com>
2024-06-19 11:44:02 +0900

    #6.0 Introduction (with error)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./styles.css"
+import "./App.module.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
