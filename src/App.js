7e1bdbfe56cf7f91b7a66c320e0964b84e80706c
hyojoo <jhj080802@naver.com>
2024-06-21 00:11:39 +0900

    #6.4 Cleanup (7:45)

--- src/App.js
+++ src/App.js
@@ -1,40 +1,26 @@
-// useEffect : 첫 번째 argument 딱 한 번만 실행시키고 싶은 코드가 됨
-// (+함수형 컴포넌트에서 부수 효과(side effects)를 수행할 수 있게 해줌)
-
 import { useState, useEffect } from "react";
 
-function App() {
-    const [counter, setValue] = useState(0);
-    const [keyword, setKeyword] = useState("");
-    const onClick = () => setValue((prev) => prev + 1);
-    const onChange = (event) => setKeyword(event.target.value);
-
-    console.log("i run all the time");
-
-    // 딱 한 번만 실행 시킴
+function Hello() {
+    // function destoryedFn() {
+    //     console.log("bye:(");
+    // }
+    // function effectFn() {
+    //     console.log("created :)");
+    // }
     useEffect(() => {
-        console.log("I run only once.");
+        console.log("created :)");
+        return () => console.log("destoryed:(");
     }, []);
-    useEffect(() => {
-        console.log("I run when 'keyword' changes.");
-    }, [keyword]);
-    useEffect(() => {
-        console.log("I run when 'counter' changes.");
-    }, [counter]);
-    useEffect(() => {
-        console.log("I run when keyword & counter changes.");
-    }, [keyword, counter]);
+    return <h1>Hello</h1>;
+}
 
+function App() {
+    const [showing, setShowing] = useState(false);
+    const onClick = () => setShowing((prev) => !prev);
     return (
         <div>
-            <input
-                value={keyword}
-                onChange={onChange}
-                type="text"
-                placeholder="Search here..."
-            />
-            <h1>{counter}</h1>
-            <button onClick={onClick}>click me</button>
+            {showing ? <Hello /> : null}
+            <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
         </div>
     );
 }
