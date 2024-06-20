fac23c0da50a07777d7ebc7564260e4ff7fb1987
hyojoo <jhj080802@naver.com>
2024-06-20 20:51:29 +0900

    #6.2 Deps (9:35)

--- src/App.js
+++ src/App.js
@@ -4,33 +4,35 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("")
-  const onClick = () => setValue((prev) => prev+1);
-  const onChange = (event) => setKeyword(event.target.value);
+    const [counter, setValue] = useState(0);
+    const [keyword, setKeyword] = useState("");
+    const onClick = () => setValue((prev) => prev + 1);
+    const onChange = (event) => setKeyword(event.target.value);
 
-  console.log("i run all the time");
+    console.log("i run all the time");
 
-  // 딱 한 번만 실행 시킴
-  useEffect(() => {
-    console.log("CALL THE API...");
-  },[]);
-  useEffect(() => {
-  console.log("SEARCH FOR", keyword);
-  }, [keyword])
+    // 딱 한 번만 실행 시킴
+    useEffect(() => {
+        console.log("I run only once.");
+    }, []);
+    useEffect(() => {
+        if (keyword !== "" && keyword.length > 5) {
+            console.log("SEARCH FOR", keyword);
+        }
+    }, [keyword]);
 
-  return (
-      <div>
-          <input
-              value={keyword}
-              onChange={onChange}
-              type="text"
-              placeholder="Search here..."
-          />
-          <h1>{counter}</h1>
-          <button onClick={onClick}>click me</button>
-      </div>
-  );
+    return (
+        <div>
+            <input
+                value={keyword}
+                onChange={onChange}
+                type="text"
+                placeholder="Search here..."
+            />
+            <h1>{counter}</h1>
+            <button onClick={onClick}>click me</button>
+        </div>
+    );
 }
 
 export default App;
