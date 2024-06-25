7508df118e9f5bfb328beb42d3384cfa005b64e7
hyojoo <jhj080802@naver.com>
2024-06-25 21:18:50 +0900

    error #7.5 React Router - start

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import{
+    react-router-dom
+npm i react-router-dom@5.3.0
+https://v5.reactrouter.com/web/guides/quick-start
+} from "react-router-dom";
+function App(){
+    return;
+}
+export default App;
